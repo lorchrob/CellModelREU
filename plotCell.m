72b6c1d8c3c21b54b396cb048a0aae3e1b53ccfd
Lorch <rlorch@iu.edu>
2019-07-01 09:46:00 -0400

    fix plotCell colors

--- plotCell.m
+++ plotCell.m
@@ -1,9 +1,9 @@
 %{
-Function to plot a cell, ideally with a red membrane and a blue interior
+Function to plot a cell with a red membrane and a blue interior
 %}
 function plotCell(cellInfo)  
   for i = 1 : numel(cellInfo.lineSegments) / 2
-     if i <= cellInfo.externalNodeCount
+     if cellInfo.lineSegments(i, 1) <= cellInfo.externalNodeCount && cellInfo.lineSegments(i, 2) <= cellInfo.externalNodeCount
        plot([cellInfo.xPosition(cellInfo.lineSegments(i, 1)), cellInfo.xPosition(cellInfo.lineSegments(i, 2))],... 
             [cellInfo.yPosition(cellInfo.lineSegments(i, 1)), cellInfo.yPosition(cellInfo.lineSegments(i, 2))],...
             'r');
