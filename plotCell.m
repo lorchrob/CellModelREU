178fe61b9c57773010a994ee551d8cddd8d6dc5f
TheAlanGan <alan.gan@outlook.com>
2019-07-12 16:07:58 -0400

    vectorized plotting. 4x faster total runtimes!!

--- plotCell.m
+++ plotCell.m
@@ -3,30 +3,26 @@ Function to plot a cell with a red membrane and a blue interior
 %}
 function plotCell(cellInfo)  
 
-  for i = 1 : numel(cellInfo.lineSegments) / 2
-     if cellInfo.lineSegments(i, 1) <= cellInfo.externalNodeCount && cellInfo.lineSegments(i, 2) <= cellInfo.externalNodeCount
-       plot([cellInfo.xPosition(cellInfo.lineSegments(i, 1)), cellInfo.xPosition(cellInfo.lineSegments(i, 2))],... 
-            [cellInfo.yPosition(cellInfo.lineSegments(i, 1)), cellInfo.yPosition(cellInfo.lineSegments(i, 2))],...
-            'r');
-       axis([-12 12 -12 12]);
-     else
-       plot([cellInfo.xPosition(cellInfo.lineSegments(i, 1)), cellInfo.xPosition(cellInfo.lineSegments(i, 2))],... 
-            [cellInfo.yPosition(cellInfo.lineSegments(i, 1)), cellInfo.yPosition(cellInfo.lineSegments(i, 2))],...
-            'b');    
-     end
-     hold on
-     axis manual
-  end
+  extLineSegs = cellInfo.externalLineSegments;
+  lineSegs = cellInfo.lineSegments;
+  IorE = logical(ismember(lineSegs, extLineSegs, 'rows') + ismember(flip(lineSegs,2), extLineSegs, 'rows'));
   
-   if isfield(cellInfo, "modelType") && cellInfo.modelType == "timeStepper"
-     for i = 1 : numel(cellInfo.xPosition) 
-       quiver(cellInfo.xPosition(i), cellInfo.yPosition(i),... 
-              cellInfo.xVelocity(i), cellInfo.yVelocity(i),...
-              0, 'k');
-       hold on
-       axis manual
-     end
-   end
+  plot( [cellInfo.xPosition(cellInfo.lineSegments(IorE, 1)), cellInfo.xPosition(cellInfo.lineSegments(IorE, 2))]',... 
+       [cellInfo.yPosition(cellInfo.lineSegments(IorE, 1)), cellInfo.yPosition(cellInfo.lineSegments(IorE, 2))]',...
+       'r');
+  axis([-20 20 -20 20])
+  hold on
+  plot([cellInfo.xPosition(cellInfo.lineSegments(~IorE, 1)), cellInfo.xPosition(cellInfo.lineSegments(~IorE, 2))]',... 
+       [cellInfo.yPosition(cellInfo.lineSegments(~IorE, 1)), cellInfo.yPosition(cellInfo.lineSegments(~IorE, 2))]',...
+       'b');
+  axis manual
+  
+  if isfield(cellInfo, "modelType") && cellInfo.modelType == "timeStepper"
+     
+    quiver(cellInfo.xPosition(:), cellInfo.yPosition(:),...
+           cellInfo.xVelocity(:), cellInfo.yVelocity(:),...
+           0, 'k');
+  end
   
   hold off
 end
