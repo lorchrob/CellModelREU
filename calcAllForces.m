771631fa5c38174b71ad65cbf7ec80245a54cf86
Lorch <rlorch@iu.edu>
2019-07-16 16:47:17 -0400

    small changes

--- calcAllForces.m
+++ calcAllForces.m
@@ -7,14 +7,24 @@ function allForces = calcAllForces(positions, cellInfo)
   
   % set positions of fixed nodes
   for i = 1 : numel(positions(:,1))
-    if cellInfo.isFixed(i)
-      positions(i,:) = [cellInfo.xPosition(i), cellInfo.yPosition(i)];
+    if cellInfo.isFixed(i, 1)
+      positions(i, 1) = cellInfo.xPosition(i);
+    end
+    
+     if cellInfo.isFixed(i, 2)
+      positions(i, 2) = cellInfo.yPosition(i);
     end
   end
   
+  % calculate forces of unfixed nodes
   for i = 1 : numel(positions(:,1))
-   if ~cellInfo.isFixed(i)
-     allForces(i,:) = calculateForce(positions, i, cellInfo); 
-   end
+    force = calculateForce(positions, i, cellInfo); 
+    if ~cellInfo.isFixed(i, 1)
+      allForces(i, 1) = force(1);
+    end
+   
+    if ~cellInfo.isFixed(i, 2)
+      allForces(i, 2) = force(2);
+    end
   end
 end
